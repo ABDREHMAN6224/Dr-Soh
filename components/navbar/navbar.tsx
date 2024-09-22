fde1c1563175f1a944d50d51144518561e58ea23
Abdul Rehman <amemon.bscs22seecs@seecs.edu.pk>
2024-09-22 17:17:05 +0500

    unused imports

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { navLinks } from "@/lib/constants/nav";
 import { LinkType } from "@/lib/types";
-import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import { FaBars } from "react-icons/fa";
